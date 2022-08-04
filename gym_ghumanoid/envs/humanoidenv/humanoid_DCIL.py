55858e241b4247ba8c60c4b88ff2a9219f169515
chenu <chenu@Alex-ISIR.local>
2022-08-04 16:59:36 +0200

    add env reward

--- gym_ghumanoid/envs/humanoidenv/humanoid_DCIL.py
+++ gym_ghumanoid/envs/humanoidenv/humanoid_DCIL.py
@@ -304,7 +304,7 @@ class GHumanoidGoal(GHumanoid, GoalEnv, utils.EzPickle, ABC):
 		self.steps += 1
 		cur_state = self.state.copy()
 
-		new_state, _, done, info =  self.env.step(action)
+		new_state, env_reward, done, info =  self.env.step(action)
 		# print("step : ", self.project_to_goal_space(new_state))
 		self.state = new_state
 		reward = self.compute_reward(self.project_to_goal_space(new_state), self.goal, {})
@@ -317,6 +317,7 @@ class GHumanoidGoal(GHumanoid, GoalEnv, utils.EzPickle, ABC):
 		truncation = truncation * (1 - is_success).reshape(1,)
 		info = {'is_success': is_success,
 				'done_from_env': np.array(done,dtype=np.intc).reshape(1,),
+				'reward_from_env': env_reward,
 				'truncation': truncation}
 		self.done = (done or bool(truncation)) or bool(is_success)
 
